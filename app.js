c19cb1fe289ff9792f982be4961ebc46c7dcc400
[Alvin <[alvin.mujkic@koredge.fr]>
2018-09-27 21:03:40 +0200

    Demineur fini :)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//div conteneur
 function Div() {
 	this.div = document.createElement('div');
 	this.div.style.cssText = 'width: 900px; height: 900px; background-color:#070707;padding: 0px; margin: 0px; position: relative';
@@ -6,6 +7,7 @@ function Div() {
 	return this.div;
 }
 
+// nom de fonciton assez parlatn, Woue
 function make2Darray(cols, rows) {
 	var array = new Array(cols);
 
@@ -20,6 +22,8 @@ var cols;
 var rows;
 var w = 45;
 
+var totalBees = 70;
+
 function setup() {
 	var div = Div(); // div globale
 	cols = 900/w;
@@ -27,17 +31,38 @@ function setup() {
 	grid = make2Darray(cols, rows);
 }
 
-function mousePressed(event) {
-	var myX = event.clientX - div.offsetLeft;
-	var myY = event.clientY- div.offsetTop;
-// console.log(typeof myX, typeof myY);
+
+function gameOver() {
 	for(var i = 0; i < cols; i++) {
 		for(var j = 0; j < rows; j++) {
-			if (grid[i][j].contains(myX, myY)) {
+			grid[i][j].reveal();
+			grid[i][j].replaceCell();
+		}
+	}
 
+}
+
+//fonction qui révèle ce qui'il y a dans la case cliquée
+function mousePressed(event) {
+
+	//on récupère les coordonnées de la souris au moment du clique, on enlève les offset, c-a-d les petites marges qu'il y a autour de la div englobante
+	var myX = event.clientX - div.offsetLeft; 
+	var myY = event.clientY- div.offsetTop;
+
+	// pour chaque cellule on vérifie si les coordonées de la souris sont dans la case
+	for(var i = 0; i < cols; i++) {
+		for(var j = 0; j < rows; j++) {
+
+			//si la souris est dans une case
+			if (grid[i][j].contains(myX, myY)) {
+				//on passe la case a reveal() { fonction qui met à jour une valeur revealed a true dont on se sert pour afficher la case cliquée (grisée ou mine)}
 				grid[i][j].reveal();
-				console.log(grid[i][j]);
 				grid[i][j].replaceCell();
+
+				if (grid[i][j].bee) {
+					gameOver();
+				}
+
 			}
 		}
 	}
@@ -47,11 +72,38 @@ function mousePressed(event) {
 
 window.onload = function() {
 	setup();
-	console.log(grid);
 	//création de la grile avec toutes les cases
 	for(var i = 0; i < cols; i++) {
 		for(var j = 0; j < rows; j++) {
-			grid[i][j] = new Cell(i*w, j*w, w);
+			grid[i][j] = new Cell(i, j, w);
+		}
+	}
+
+	//tableau contenant tous les enplacement de cellules, on va piocher dans ce tableua 10 des emplacment au hasard pour positionner les mines
+	var options = [];
+	for(var i = 0; i < cols; i++) {
+		for(var j = 0; j < rows; j++) {
+			options.push([i, j]);
+		}
+	}
+
+	// positionne les mines dans des cellules au hasard
+	for(var n = 0; n < totalBees; n++) {
+		var index = Math.floor((Math.random() * options.length));// on récup-re un index au hasard dans le tableau de toutes les cellules
+
+		// on récupère le i et le j de la cellule au hasard choisie
+		var choice = options[index]; 
+		var i = choice[0]; 
+		var j = choice[1]; 
+
+		options.splice(index, 1); // on retir la cellules choisie du tableua, ca évite de positionner 2 mines dans la même cellule
+		grid[i][j].bee = true; // on attribu la mine à la cellule hasardeuse
+	}
+
+	// pour chaque grille on compte le nombre de mines voisines et on met à jour la valeur bee (guêpe, mine) de la cellule
+	for(var i = 0; i < cols; i++) {
+		for(var j = 0; j < rows; j++) {
+			grid[i][j].countBees();
 		}
 	}
 
@@ -62,5 +114,6 @@ window.onload = function() {
 		}
 	}
 
+	//appel de la fonciton quandon clique sur une case
 	div.addEventListener('click', mousePressed);
 }
--- cell.js
+++ cell.js
@@ -1,19 +1,56 @@
-function Cell(x, y, w) {
+function Cell(i, j, w) {
 
-	if (Math.random(1) < 0.33) {
-		this.bee = true;	
-	} else {
-		this.bee = false;
-	}
+	// if (Math.random(1) < 0.33) {
+	// 	this.bee = true;	
+	// } else {
+	// 	this.bee = false;
+	// }
+
+	this.bee = false;
 	
 	this.revealed = false;
 
-	this.x = x;
-	this.y = y;
-	this.w = w;
-	this.id_div = this.x+'_'+this.y;
+	this.i = i; // indexs
+	this.j = j; // indexs
+	this.x = i*w; // marges, position en px
+	this.y = j*w; // marges, position en px
+	this.w = w; // taille cellule
+	this.id_div = this.x+'_'+this.y; // id d'une div, ca peut tjr servir
+
+	this.countVoisin = 0;
 }
 
+//fonction qui compte les voisins mines (abeille) de la case cochée
+Cell.prototype.countBees = function() {
+
+	// si la case est une mine, on ne compte pas
+	if (this.bee) {
+		this.countVoisin = -1;
+		return;
+	}
+
+	//pour chaque case, on vérifie ses idex voisin horizontaux et verticaux pour voir si il y a des case voisine avec des mines
+	var total = 0;
+	for(var xoff = -1; xoff <= 1; xoff++) { 
+		for(var yoff = -1; yoff <= 1; yoff++ ) {
+
+			var i = this.i + xoff;
+			var j = this.j + yoff;
+			
+			if (i > -1 && i < cols && j > -1 && j < rows) {
+				var voisin = grid[i][j];
+				// si la case voisine a une mine (bee : abeille) , on oincrémente le compte total des voisin contenant des mines
+				if (voisin.bee) {
+					total++;
+				}
+			}
+				
+		}
+	}
+	this.countVoisin = total;
+}
+
+//afichage de toutes les cases
 Cell.prototype.show = function() {
 	var x = this.x;
 	var y = this.y;
@@ -60,6 +97,7 @@ Cell.prototype.show = function() {
 	div.appendChild(this.child_div);
 }
 
+//fonciton qui remplae une case non dévoilée, par une case dévoilé et ce qu'il y a l'intérieur (mine ou chiffre)
 Cell.prototype.replaceCell = function() {
 	var elem = document.getElementById(this.id_div);
 	elem.parentNode.removeChild(elem);
@@ -100,7 +138,21 @@ Cell.prototype.replaceCell = function() {
 						margin-top:'+y+'px;\
 			 			width:'+(w -1)+'px;\
 			 			height:'+(w -1)+'px;\
-			 		';
+			 		';	
+			if (this.countVoisin > 0) {
+				this.child_div.innerHTML = '\
+							<div style="width: 100%;\
+							height: 100%;\
+							text-align: center;\
+							align-items: center;\
+							line-height: 46px;\
+							font-weight: bold;\
+							font-size: 30px;">'
+								+this.countVoisin+
+							'</div>\
+						';
+			}		
+			
 		}
 	}
 	this.child_div.setAttribute("id", this.id_div);
@@ -108,13 +160,39 @@ Cell.prototype.replaceCell = function() {
 	div.appendChild(this.child_div);
 }
 
+//check si le clique de la souris est dans la case actuelle
 Cell.prototype.contains = function(x, y) {
 	return (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.w);
 }
 
+//met a jour la visibilité d'une case
 Cell.prototype.reveal = function() {
 	this.revealed = true;
+
+	if (this.countVoisin === 0) {
+		//une fois une case cliqée et révélée, si elle a 0 voisin mine => propagation pour afficher toutes les case voisine sans voisin min en boucle
+		this.propagation();
+	}
 	
+}
+
+Cell.prototype.propagation = function() {
+	//pour chaque case, on vérifie ses idex voisin horizontaux et verticaux pour voir ses case voisines
+	var total = 0;
+	for(var xoff = -1; xoff <= 1; xoff++) { 
+		for(var yoff = -1; yoff <= 1; yoff++ ) {
+			var i = this.i + xoff;
+			var j = this.j + yoff;			
+			if (i > -1 && i < cols && j > -1 && j < rows) {
+				var voisin = grid[i][j];
+				// si la case voisine a une mine (bee : abeille) , on oincrémente le compte total des voisin contenant des mines
+				if (!voisin.bee && !voisin.revealed) {
+					voisin.reveal();
+					voisin.replaceCell();
+				}
+			}
+		}
+	}
 }
 
 
